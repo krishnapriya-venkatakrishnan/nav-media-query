734616ce5526842128364d0bac92e9731aace3f6
Krishnapriya Venkatakrishnan <144596138+krishnapriya-venkatakrishnan@users.noreply.github.com>
2024-08-07 15:21:40 +0200

    Removing unwanted comments
    Removing unwanted comments

--- index.js
+++ index.js
@@ -19,12 +19,10 @@ exit.addEventListener('click', () => {
 
 let width = document.getElementById('width');
 var onresize = function() {
-   //your code here
-   //this is just an example
-   width.innerText = document.body.clientWidth;
-   width.classList.add('display-width');
-   setTimeout(() => {
-       width.classList.remove('display-width');
-   }, 2000)
+    width.innerText = document.body.clientWidth;
+    width.classList.add('display-width');
+    setTimeout(() => {
+        width.classList.remove('display-width');
+    }, 2000)
 }
 window.addEventListener("resize", onresize);
